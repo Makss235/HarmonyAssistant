42d86b6584b8ee0d20b1fbb7b80c0a4ef70c6776
Makss <maks.shirokov.04@inbox.ru>
2023-08-21 19:03:23 +0300

    Базовая разметка главного окна

--- HarmonyAssistant/UI/Windows/MainWindow/MainWindow.cs
+++ HarmonyAssistant/UI/Windows/MainWindow/MainWindow.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace HarmonyAssistant.UI.Windows.MainWindow
 {
@@ -11,7 +13,32 @@ namespace HarmonyAssistant.UI.Windows.MainWindow
 
         private void InitializeComponent()
         {
+            Border border = new Border();
+            border.Background = Brushes.Black;
+            border.Width = 40;
+            Grid.SetColumn(border, 0);
+            Grid.SetRowSpan(border, 2);
+
+            ColumnDefinition columnDefinition = new ColumnDefinition()
+            { Width = new GridLength(1, GridUnitType.Auto) };
+
+            ColumnDefinition columnDefinition1 = new ColumnDefinition()
+            { Width = new GridLength(1, GridUnitType.Star) };
+
+            RowDefinition rowDefinition = new RowDefinition()
+            { Height = new GridLength(40, GridUnitType.Pixel) };
             
+            RowDefinition rowDefinition1 = new RowDefinition()
+            { Height = new GridLength(1, GridUnitType.Star) };
+
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(columnDefinition);
+            grid.ColumnDefinitions.Add(columnDefinition1);
+            grid.RowDefinitions.Add(rowDefinition);
+            grid.RowDefinitions.Add(rowDefinition1);
+            grid.Children.Add(border);
+
+            Content = grid;
         }
     }
 }
