b72cf9e8071e731df47938af75fb94d54e3595bc
Makss <maks.shirokov.04@inbox.ru>
2023-08-29 21:37:00 +0300

    Промежуточный

---
+++ HarmonyAssistant/UI/Windows/MainWindow/Widgets/Tabs/SettingsTab/SettingsTab.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.Base;
+
+namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
+{
+    public class SettingsTab : Tab
+    {
+        public SettingsTab()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
