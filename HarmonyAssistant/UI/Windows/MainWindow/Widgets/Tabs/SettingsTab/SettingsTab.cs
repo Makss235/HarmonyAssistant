717bd447b92402c4fa0ce9cb82b0bd91446fe59c
MrVeseR <ViValKuzya1107@gmail.com>
2023-09-08 19:43:56 +0300

    Сделал внешний вид для профиля в настройках

--- HarmonyAssistant/UI/Windows/MainWindow/Widgets/Tabs/SettingsTab/SettingsTab.cs
+++ HarmonyAssistant/UI/Windows/MainWindow/Widgets/Tabs/SettingsTab/SettingsTab.cs
@@ -3,10 +3,10 @@ using HarmonyAssistant.UI.Windows.MainWindow.Styles;
 using HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.Base;
 using HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tools;
 using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
 {
@@ -23,6 +23,51 @@ namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
             tabAppearAnim = new TabAppearAnim(this);
             IsVisibleChanged += SettingsTab_IsVisibleChanged;
 
+            Image userImage = new Image()
+            {
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Margin = new Thickness(10, 10, 7, 10),
+                Width = 65,
+                Height = 65,
+                Source = new BitmapImage(
+                    new Uri("pack://application:,,,/Data/Resources/Images/Icon.png",
+                    UriKind.RelativeOrAbsolute)),
+            };
+
+            UserTable userTable = new UserTable(userImage, "Пиписьк Яблонович");
+            Grid.SetColumn(userTable, 0);
+            Grid.SetRow(userTable, 0);
+            Grid.SetColumnSpan(userTable, 2);
+
+            TextBlock themeTB = new TextBlock()
+            {
+                Text = "Тема",
+                Style = TextBlocksStyles.textBlockStyle,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(10, 10, 7, 10)
+            };
+            Grid.SetColumn(themeTB, 0);
+            Grid.SetRow(themeTB, 1);
+            
+            ThemeButton themeBRed = new ThemeButton(Brushes.Red);
+            themeBRed.PreviewMouseLeftButtonDown += (s, e) => ProgramBrushes.LessDarkBlue = Brushes.Green;
+            ThemeButton themeBYellow = new ThemeButton(Brushes.Yellow);
+
+            WrapPanel themeStackPanel = new WrapPanel()
+            { 
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(10, 10, 7, 10),
+                Children =
+                {
+                    themeBRed,
+                    themeBYellow,
+                }
+            };
+            Grid.SetColumn(themeStackPanel, 1);
+            Grid.SetRow(themeStackPanel, 1);
+
             TextBlock soundTB = new TextBlock()
             {
                 Text = "Звук",
@@ -31,8 +76,7 @@ namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
                 Margin = new Thickness(10, 10, 7, 10)
             };
             Grid.SetColumn(soundTB, 0);
-            Grid.SetRow(soundTB, 0);
-
+            Grid.SetRow(soundTB, 2);
 
             Slider soundSlider = new Slider()
             {
@@ -44,36 +88,8 @@ namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
                 Margin = new Thickness(7, 0, 10, 0),
             };
             Grid.SetColumn(soundSlider, 1);
-            Grid.SetRow(soundSlider, 0);
+            Grid.SetRow(soundSlider, 2);
 
-            TextBlock themeTB = new TextBlock()
-            {
-                Text = "Тема",
-                Style = TextBlocksStyles.textBlockStyle,
-                VerticalAlignment = VerticalAlignment.Center,
-                Margin = new Thickness(10, 10, 7, 10)
-            };
-            Grid.SetColumn(themeTB, 0);
-            Grid.SetRow(themeTB, 1);
-
-            
-            ThemeButton themeBRed = new ThemeButton(Brushes.Red);
-            themeBRed.PreviewMouseLeftButtonDown += (s, e) => ProgramBrushes.LessDarkBlue = Brushes.Green;
-            ThemeButton themeBYellow = new ThemeButton(Brushes.Yellow);
-
-            WrapPanel themeStackPanel = new WrapPanel()
-            { 
-                Orientation = Orientation.Horizontal,
-                HorizontalAlignment = HorizontalAlignment.Center,
-                Margin = new Thickness(10, 10, 7, 10),
-                Children =
-                {
-                    themeBRed,
-                    themeBYellow,
-                }
-            };
-            Grid.SetColumn(themeStackPanel, 1);
-            Grid.SetRow(themeStackPanel, 1);
 
             ColumnDefinition leftColumnDfn = new ColumnDefinition()
             { Width = new GridLength(1, GridUnitType.Auto) };
@@ -84,7 +100,8 @@ namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
             { Height = new GridLength(1, GridUnitType.Auto) };
             RowDefinition themeRowDfn = new RowDefinition()
             { Height = new GridLength(1, GridUnitType.Auto) };
-
+            RowDefinition userRowDfn = new RowDefinition()
+            { Height = new GridLength(1, GridUnitType.Auto) };
 
             Grid mainGrid = new Grid() 
             { HorizontalAlignment = HorizontalAlignment.Stretch };
@@ -92,10 +109,12 @@ namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
             mainGrid.ColumnDefinitions.Add(rightColumnDfn);
             mainGrid.RowDefinitions.Add(soundRowDfn);
             mainGrid.RowDefinitions.Add(themeRowDfn);
+            mainGrid.RowDefinitions.Add(userRowDfn);
             mainGrid.Children.Add(soundTB);
             mainGrid.Children.Add(soundSlider);
             mainGrid.Children.Add(themeTB);
             mainGrid.Children.Add(themeStackPanel);
+            mainGrid.Children.Add(userTable);
 
             ResourceDictionary svStyle = new ResourceDictionary()
             {
@@ -111,10 +130,8 @@ namespace HarmonyAssistant.UI.Windows.MainWindow.Widgets.Tabs.SettingsTab
 
         private void SettingsTab_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (this.Visibility == Visibility.Visible)
-            {
+            if (Visibility == Visibility.Visible)
                 tabAppearAnim.StartAnim();
-            }
         }
     }
 }
